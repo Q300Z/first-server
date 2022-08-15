8bc9fd76d93b2b12470d8d95bd4d6e5c23fcebb9
Q300Z <Q300Zhomas@gmail.com>
2022-08-15 18:46:54 +0200

    first commit

---
+++ first-front/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Multicards :cards="this.cards" />
+  </div>
+</template>
+
+<script>
+import Multicards from "./components/cards.vue";
+//
+export default {
+  name: "App",
+  components: {
+    Multicards,
+  },
+  data: function () {
+    return {
+      cards: [],
+    };
+  },
+  created() {
+    fetch("http://localhost:3000/api/stuff")
+      .then((response) => response.json())
+      .then((data) => (this.cards = data));
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ first-front/src/components/cards.vue
@@ -0,0 +1,28 @@
+<template>
+  <dir>
+    <div class="cards" v-for="card in cards" :key="card.id">
+      <v-card elevation="5">
+        <v-card-title>{{ card.title }}</v-card-title>
+        <v-card-text>{{ card.description }}</v-card-text>
+        {{ card.price }}€
+        <v-card-subtitle>{{ card._id }} - {{ card.userId }}</v-card-subtitle>
+      </v-card>
+    </div>
+  </dir>
+</template>
+
+<script>
+export default {
+  name: "Multi-cards",
+  props: ["cards"],
+};
+</script>
+<style>
+.cards {
+  max-height: 150px;
+  max-width: 300px;
+  text-align: center;
+  margin: 10px;
+  display: inline-block;
+}
+</style>
